ab11b35a8a14564e138923a5a62342ffbb2826d2
ecnopronin <ecnopronin@yahoo.com>
2020-11-30 11:55:21 +0800

    commit new file calc.go with added Subtract function

--- calc.go
+++ calc.go
@@ -4,3 +4,8 @@ package calc
 func Add(addend1, addend2 int) int {
 	return addend1 + addend2
 }
+
+// Subtract function to get difference of 2 integers
+func Subtract(minuend, subtrahend int) int {
+	return minuend - subtrahend
+}
